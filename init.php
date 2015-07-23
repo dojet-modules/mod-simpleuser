c670ab5a7ff6f2f6356aba7bd60c0ec7b06b5dd5
setimouse <setimouse@gmail.com>
2015-07-23 10:57:47 +0800

    init

--- init.php
+++ init.php
@@ -1,8 +1,7 @@
 <?php
-require __DIR__.'/ModuleSimpleUser.class.php';
-
 DAutoloader::getInstance()->addAutoloadPathArray(
     array(
+        dirname(__FILE__).'/',
         dirname(__FILE__).'/lib/',
         dirname(__FILE__).'/dal/',
         dirname(__FILE__).'/model/',
